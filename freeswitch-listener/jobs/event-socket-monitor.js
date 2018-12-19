7fd03ec71b432c6a4358ffb3cb98473bff3fe255
Paolo Rovelli <rovellipaolo@gmail.com>
2018-12-19 22:44:13 +0100

    Add listeners to CLOSE and ERROR connection events

--- freeswitch-listener/jobs/event-socket-monitor.js
+++ freeswitch-listener/jobs/event-socket-monitor.js
@@ -24,6 +24,18 @@ const startJob = () => {
 				ChannelObserver.notify(channelEvent);
 			}
 		});
+
+		/*
+		connection.on(FreeswitchApi.Event.Connection.CLOSED, () => {
+		    Log.e(`Connection to FreeSWITCH was closed. Retrying in ${RETRY_TIMEOUT_IN_MILLIS} millis...`);
+		    setTimeout(startJob, RETRY_TIMEOUT_IN_MILLIS);
+		});
+
+		connection.on(FreeswitchApi.Event.Connection.ERROR, () => {
+		    Log.e(`Error connecting to FreeSWITCH. Retrying in ${RETRY_TIMEOUT_IN_MILLIS} millis...`);
+		    setTimeout(startJob, RETRY_TIMEOUT_IN_MILLIS);
+		});
+		*/
 	})
 	.catch(() => {
 		Log.error(`Error connecting to FreeSWITCH. Retrying in ${RETRY_TIMEOUT_IN_MILLIS} millis...`);
